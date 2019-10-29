5606357408eadf6b9081b75618024fd9174bb82d
SebastianGiovagnoni <47358975+SebastianGiovagnoni@users.noreply.github.com>
2019-10-29 11:38:37 -0300

    Add files via upload

---
+++ Win_64/LinkedList.h
@@ -0,0 +1,72 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2018>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __LINKEDLIST
+#define __LINKEDLIST
+struct Node
+{
+    void* pElement;
+    struct Node* pNextNode;
+}typedef Node;
+
+struct LinkedList
+{
+    Node* pFirstNode;
+    int size;
+}typedef LinkedList;
+#endif
+
+
+
+//Publicas
+LinkedList* ll_newLinkedList(void);//nueva lista
+
+int ll_len(LinkedList* this);//tamaño
+
+Node* test_getNode(LinkedList* this, int nodeIndex);
+int test_addNode(LinkedList* this, int nodeIndex,void* pElement);//testeos
+
+int ll_add(LinkedList* this, void* pElement);//agregar cualquier tipo de dato
+
+void* ll_get(LinkedList* this, int index);//buscar por indice
+
+int ll_set(LinkedList* this, int index,void* pElement);//poner un elemento arriba de otro o reemplazar
+
+int ll_remove(LinkedList* this,int index);//saca un elemento de la lista
+
+int ll_clear(LinkedList* this);//borra los elementos de la lista
+
+int ll_deleteLinkedList(LinkedList* this);//borra la lista con todos los elementos
+
+int ll_indexOf(LinkedList* this, void* pElement);//paso un elemento y me dice en que indice esta
+
+int ll_isEmpty(LinkedList* this);//dice si hay algo ne la lista o no
+
+int ll_push(LinkedList* this, int index, void* pElement);//agrega un indice a la lista sin pisar al anterior
+
+void* ll_pop(LinkedList* this,int index);//elimina pero antes de borrarlo lo guarda y lo devuelve
+
+int ll_contains(LinkedList* this, void* pElement);//paso el elemento y me dice si esta el elemento en la lista
+
+int ll_containsAll(LinkedList* this,LinkedList* this2);//sub lista si todos los elementos de la primer lista estan en la segunda
+
+LinkedList* ll_subList(LinkedList* this,int from,int to);//crea una sub lista hasta donde quieras
+
+LinkedList* ll_clone(LinkedList* this);//crea una copia identica de la lista
+
+int ll_sort(LinkedList* this, int (*pFunc)(void* ,void*), int order);//ordena
---
+++ Win_64/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.bin (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.bin (modo binario).
+    10. Salir
+*****************************************************/
+
+
+int main()
+{
+    //int option = 0;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+
+    Employee* e1 = employee_newParametros("5", "Maria", "10", "5000");
+    Employee* e2 = employee_newParametros("6", "Carlos", "11", "6000");
+    Employee* e3 = employee_newParametros("7", "Sebastian", "12", "7000");
+    Employee* e4 = employee_newParametros("8", "Juan", "20", "100");
+
+    Employee* aux;
+    int x;
+    int len;
+
+    ll_add(listaEmpleados,e1);
+    ll_add(listaEmpleados,e2);
+    ll_add(listaEmpleados,e3);
+    ll_add(listaEmpleados,e4);
+
+    len = ll_len(listaEmpleados);
+
+    for(x=0; x<len; x++)
+    {
+        aux = (Employee*) ll_get(listaEmpleados,x);
+        mostrarEmpleado(aux);
+    }
+    ll_pop(listaEmpleados,3);
+    printf("\neliminaste a este: \n");
+
+    mostrarEmpleado(aux);
+    printf("\n");
+
+    len=ll_len(listaEmpleados);
+
+    for(x=0; x<len; x++)
+    {
+        aux = (Employee*) ll_get(listaEmpleados,x);
+        mostrarEmpleado(aux);
+    }
+    len = ll_isEmpty(listaEmpleados);
+    printf("%d", len);
+    ll_clear(listaEmpleados);
+
+   LinkedList* listaAux = ll_len(listaEmpleados);
+     for(x=0; x<len; x++)
+    {
+        aux = (Employee*) ll_get(listaAux,x);
+        mostrarEmpleado(aux);
+    }
+
+    return 0;
+}
